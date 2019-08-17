270950c1a3b10a97b0c9b98cd294a394cd33c1a5
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2019-08-17 10:11:23 -0400

    adding a file

---
+++ public/javascript/signin.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+
+    $("#log-in").on("click", function () {
+
+        var user = $("#user-input").val().trim();
+        var password = $("#pass-input").val().trim();
+        
+
+    });
+
+});
