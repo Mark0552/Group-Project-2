0603b2df6ac4ab6216f4493a66486d17163507a6
IzzyD <davidirizarry@Davids-MacBook-Pro.local>
2019-08-24 12:11:00 -0400

    pages styled

--- public/javascript/clothing.js
+++ public/javascript/clothing.js
@@ -10,16 +10,18 @@
   });
 
 
-$("#carouselRow").hide(200);
-$("#carouselRow2").hide(200);
-$("#carouselRow3").hide(200);
-$("#carouselRow4").hide(200);
-
+$("#carouselRow, #carouselRow2, #carouselRow3, #carouselRow4").hide(200);
   $("#myBtn, #silhouette").on("click", function () {
-    $("#carouselRow, #carouselRow2, #carouselRow3, #carouselRow4").delay(3000).fadeIn('slow');
+    $('#carouselRow').delay(3000).fadeIn('slow');
+    $('#carouselRow2').delay(3000).fadeIn('slow');
+    $('#carouselRow3').delay(3000).fadeIn('slow');
+    $('#carouselRow4').delay(3000).fadeIn('slow');
+    $('#carouselRow5').delay(3000).fadeIn('slow');
     $("#myBtnRow, #silhouette").hide(2000).fadeout('slow');
     M.AutoInit();
 
 });
+
+
 
 
